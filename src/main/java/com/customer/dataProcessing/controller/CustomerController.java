2af6cb2f45efd5f3a01415fc91e13023ecf4a3bf
unknown <gmsaiful71@gmail.com.com>
2022-11-27 02:03:27 +0600

    API path change

--- src/main/java/com/customer/dataProcessing/controller/CustomerController.java
+++ src/main/java/com/customer/dataProcessing/controller/CustomerController.java
@@ -19,7 +19,7 @@ public class CustomerController {
     @Autowired
     private CustomerService service;
 
-    @PostMapping(value = "/users", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = "application/json")
+    @PostMapping(value = "/customers", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = "application/json")
     public ResponseEntity saveCustomers(@RequestParam(value = "files") MultipartFile[] files) throws Exception {
         for (MultipartFile file : files) {
             service.saveData(file);
@@ -27,7 +27,7 @@ public class CustomerController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping(value = "/getCSV", produces = "application/json")
+    @GetMapping(value = "/customers", produces = "application/json")
     public CompletableFuture<ResponseEntity> getCSV() throws IOException {
         return  service.getCSV().thenApply(ResponseEntity::ok);
     }
